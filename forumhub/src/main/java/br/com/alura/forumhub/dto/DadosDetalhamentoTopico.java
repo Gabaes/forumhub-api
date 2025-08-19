1fa97343bf80006d2786f79cec1f3287fa79160b
Gabaes <gabrielmine.gg@gmail.com>
2025-08-19 13:42:15 -0400

    Initial commit

---
+++ forumhub/src/main/java/br/com/alura/forumhub/dto/DadosDetalhamentoTopico.java
@@ -0,0 +1,10 @@
+package br.com.alura.forumhub.dto;
+
+import br.com.alura.forumhub.model.Topico;
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoTopico(Long id, String titulo, String mensagem, LocalDateTime dataCriacao, boolean status, String autor, String curso) {
+    public DadosDetalhamentoTopico(Topico topico) {
+        this(topico.getId(), topico.getTitulo(), topico.getMensagem(), topico.getDataCriacao(), topico.isStatus(), topico.getAutor(), topico.getCurso());
+    }
+}
---
+++ forumhub/src/main/java/br/com/alura/forumhub/model/Topico.java
@@ -0,0 +1,30 @@
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "Topico")
+@Table(name = "topicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+    private boolean status = true;
+    private String autor;
+    private String curso;
+
+    public Topico(DadosCadastroTopico dados) {
+        this.titulo = dados.titulo();
+        this.mensagem = dados.mensagem();
+        this.autor = dados.autor();
+        this.curso = dados.curso();
+    }
+}
